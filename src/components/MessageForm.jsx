d4560166b47f3d626045b248ba3b64abe9aa9158
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-20 22:42:42 -0400

    Send image in the chat

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
   const [value, setValue] = useState('');
@@ -8,6 +9,10 @@ const MessageForm = (props) => {
   const handleChange = (e) => {
     setValue(e.target.value);
     isTyping(props, chatId);
+  };
+
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: '' });
   };
 
   const handleSubmit = (e) => {
@@ -29,6 +34,21 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor='upload-button'>
+        <span className='image-button'>
+          <PictureOutlined className='picture-icon' />
+        </span>
+      </label>
+      <input
+        type='file'
+        multiple={false}
+        id='upload-button'
+        style={{ display: 'none' }}
+        onChange={handleUpload}
+      />
+      <button type='submit' className='send-button'>
+        <SendOutlined className='send-icon' />
+      </button>
     </form>
   );
 };
