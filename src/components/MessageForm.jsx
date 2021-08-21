3243fd968e38356bb69371840148218628d5e471
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-20 22:30:05 -0400

    Send a message functionnality

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,36 @@
-const MessageForm = () => {
-  return <div>Message Form</div>;
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const text = value.trim();
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+      setValue('');
+    }
+  };
+
+  return (
+    <form className='message-form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='message-input'
+        placeholder='Send a Message'
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
+  );
 };
 
 export default MessageForm;
